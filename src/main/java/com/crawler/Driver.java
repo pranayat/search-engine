277b6d8e7bbdfc3a2f61e0389528cbe060c102f5
pranayat <pranayatomar@gmail.com>
2021-11-21 15:51:01 +0100

    Update readme

--- src/main/java/com/crawler/Driver.java
+++ src/main/java/com/crawler/Driver.java
@@ -75,28 +75,29 @@ public class Driver {
 	
 	public static void main(String[] args) {
 		
+		// Comment these out if this is not the first time runningthis project
 		dropTables();
 		createTables();
 		
 		Crawler c1 = new Crawler(false, 1, 10, 100, 100, "https://en.wikipedia.org/wiki/Led_Zeppelin");
 		Thread crawler1 = new Thread(c1);
-//		Crawler c2 = new Crawler(false, 2, 10, 100, 100, "https://cs.uni-kl.de");
-//		Thread crawler2 = new Thread(c2);
-//		Crawler c3 = new Crawler(false, 3, 10, 100, 100, "https://www.uni-kl.de");		
-//		Thread crawler3 = new Thread(c3);
-//		Crawler c4 = new Crawler(false, 3, 10, 100, 100, "https://www.kaiserslautern.de");		
-//		Thread crawler4 = new Thread(c4);
+		Crawler c2 = new Crawler(false, 2, 10, 100, 100, "https://cs.uni-kl.de");
+		Thread crawler2 = new Thread(c2);
+		Crawler c3 = new Crawler(false, 3, 10, 100, 100, "https://www.uni-kl.de");		
+		Thread crawler3 = new Thread(c3);
+		Crawler c4 = new Crawler(false, 3, 10, 100, 100, "https://www.kaiserslautern.de");		
+		Thread crawler4 = new Thread(c4);
 		
 		crawler1.start();
-//		crawler2.start();
-//		crawler3.start();
-//		crawler4.start();
+		crawler2.start();
+		crawler3.start();
+		crawler4.start();
 		
 		try {
 			crawler1.join();
-//			crawler2.join();
-//			crawler3.join();
-//			crawler4.join();
+			crawler2.join();
+			crawler3.join();
+			crawler4.join();
 			System.out.println("END");
 		} catch (InterruptedException e) {
 			e.printStackTrace();
