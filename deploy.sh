f0a82204a58dd715b00c507f6d08e2bb2606160f
pranayat <pranayatomar@gmail.com>
2021-12-11 17:53:22 +0100

    update execute permissions in script

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,11 @@ sudo chown -R project:project *
 
 cd /home/project/group-03
 git pull origin main
+
+chmod u+x deploy.sh
+chomd u+x crawl.sh
+chmod u+x first_crawl.sh
+
 sudo service tomcat9 stop
 sudo cp /home/project/group-03/CrawlSearch.war /var/lib/tomcat9/webapps/
 sudo service tomcat9 start
