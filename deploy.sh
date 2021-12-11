0d0c87b0cdbdfb0597f5303b71d3d455983bdae6
pranayat <pranayatomar@gmail.com>
2021-12-11 17:43:18 +0100

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+eval "$(ssh-agent -s)"
+ssh-add -k /home/project/.ssh/y
+cd /home/project/group-03/.git/objects
+sudo chown -R project:project *
+
+cd /home/project/group-03
+git pull origin main
+sudo service tomcat9 stop
+sudo cp /home/project/group-03/CrawlSearch.war /var/lib/tomcat9/webapps/
+sudo service tomcat9 start
