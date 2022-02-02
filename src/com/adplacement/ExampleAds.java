c7df8e3ed0868e3aaf531bc337497e69127af96a
pranayat <pranayatomar@gmail.com>
2022-02-02 18:30:52 +0100

    change set code to make it compatible with java 1.8

--- src/com/adplacement/ExampleAds.java
+++ src/com/adplacement/ExampleAds.java
@@ -1,6 +1,9 @@
 package com.adplacement;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ExampleAds {
 	
@@ -22,9 +25,10 @@ public class ExampleAds {
 		String firstname = "Christoph";
 		String lastname = "Lossen";
 		String url = "https://www.mathematik.uni-kl.de/organisation/dekanat/lossen";
-		String text = "Unser Übungsbetrieb funktioniert nur, wenn wir genügend motivierte und gut qualifizierte Übungsleiter an unserem Fachbereich haben. Deshalb suchen wir Sie! Bewerben Sie sich jetzt!";
+		String text = "Unser ï¿½bungsbetrieb funktioniert nur, wenn wir genï¿½gend motivierte und gut qualifizierte ï¿½bungsleiter an unserem Fachbereich haben. Deshalb suchen wir Sie! Bewerben Sie sich jetzt!";
 		String iurl = "https://www.mathematik.uni-kl.de/fileadmin/_processed_/a/4/csm_Lossen_1_zu_1_9ea7dd430c.jpg";
-		Set <String> ngrams = Set.<String>of("übungsleiter", "lehre","übungsbetrieb","job","bewerben");
+		Set <String> ngrams = Stream.of("ï¿½bungsleiter", "lehre","ï¿½bungsbetrieb","job","bewerben")
+		         .collect(Collectors.toCollection(HashSet::new)); 
 		float budget = (float)12.0;
 		float onclick = (float)0.05;
 		String language = "ger";
@@ -36,7 +40,8 @@ public class ExampleAds {
 		url = "https://www.mathematik.uni-kl.de/en/organisation/dean-office/lossen";
 		text = "Our exercise operation only works if we have enough motivated and well-qualified exercise instructors on our faculty. That is why we are looking for you! Apply now!";
 		iurl = "https://www.mathematik.uni-kl.de/fileadmin/_processed_/a/4/csm_Lossen_1_zu_1_9ea7dd430c.jpg";
-		ngrams = Set.<String>of("instructor", "teaching","apply","job","offers");
+		ngrams = Stream.of("instructor", "teaching","apply","job","offers")
+		         .collect(Collectors.toCollection(HashSet::new)); 
 		budget = (float)6.0;
 		onclick = (float)0.05;
 		language = "eng";
@@ -49,7 +54,8 @@ public class ExampleAds {
 		url = "https://modhb.uni-kl.de/";
 		text = "Check out our new digital module handbook. There you can find all necessary information about your study.";
 		iurl = "";
-		ngrams = Set.<String>of("module handbook", "teaching","study","degree");
+		ngrams = Stream.of("module handbook", "teaching","study","degree")
+		         .collect(Collectors.toCollection(HashSet::new));
 		budget = (float)20.0;
 		onclick = (float)0.01;
 		language = "eng";
@@ -60,9 +66,10 @@ public class ExampleAds {
 		firstname = "SCI";
 		lastname = "KL";
 		url = "https://www.cs.uni-kl.de/en/studium/studierende/hiwis/";
-		text = "The department of computer science is offering student assistant jobs. Let’s check them out.";
+		text = "The department of computer science is offering student assistant jobs. Letï¿½s check them out.";
 		iurl = "";
-		ngrams = Set.<String>of("computer science", "job","sci");
+		ngrams = Stream.of("computer science", "job","sci")
+		         .collect(Collectors.toCollection(HashSet::new));
 		budget = (float)5.0;
 		onclick = (float)0.01;
 		language = "eng";
@@ -75,7 +82,8 @@ public class ExampleAds {
 		url = "https://www.topuniversities.com/courses/computer-science-information-systems/guide";
 		text = "What is a computer science degree? What do you learn? Why should you apply? And which are the best universities? Find out more on our page.";
 		iurl = "";
-		ngrams = Set.<String>of("computer science", "degree");
+		ngrams = Stream.of("computer science", "degree")
+		         .collect(Collectors.toCollection(HashSet::new));
 		budget = (float)1;
 		onclick = (float)0.2;
 		language = "eng";
@@ -88,7 +96,8 @@ public class ExampleAds {
 		url = "https://compscicentral.com/best-gear-for-computer-science-students/";
 		text = "If you want to study computer science you have to think about your equipment. We have complied the most important stuff.";
 		iurl = "https://compscicentral.com/wp-content/uploads/2019/11/BestGear-1.jpg?ezimgfmt=ng%3Awebp%2Fngcb1%2Frs%3Adevice%2Frscb1-1";
-		ngrams = Set.<String>of("computer science", "equipment");
+		ngrams = Stream.of("computer science", "equipment")
+		         .collect(Collectors.toCollection(HashSet::new));
 		budget = (float)1;
 		onclick = (float)0.1;
 		language = "eng";
@@ -101,7 +110,8 @@ public class ExampleAds {
 		url = "https://dbis.informatik.uni-kl.de/index.php/en/teaching/winter-21-22/is-project-21-22";
 		text = "In this project, we consider building a full-fledged Web Search engine. If you are interested apply until tomorrow.";
 		iurl = "";
-		ngrams = Set.<String>of("computer science", "project", "db", "dbislab");
+		ngrams = Stream.of("computer science", "project", "db", "dbislab")
+		         .collect(Collectors.toCollection(HashSet::new));
 		budget = (float)6;
 		onclick = (float)0.2;
 		language = "eng";
@@ -114,7 +124,8 @@ public class ExampleAds {
 		url = "https://nlp.stanford.edu/IR-book/";
 		text = "You want to learn the basics of information systems. Check out this book. Also consider other literature.";
 		iurl = "https://nlp.stanford.edu/IR-book/iir.jpg";
-		ngrams = Set.<String>of("computer science", "literature","information systems", "dbislab");
+		ngrams = Stream.of("computer science", "literature","information systems", "dbislab")
+		         .collect(Collectors.toCollection(HashSet::new));
 		budget = (float)1.0;
 		onclick = (float)0.3;
 		language = "eng";
@@ -128,7 +139,8 @@ public class ExampleAds {
 		text = "If you are a student at TU Kaiserslautern and in the phase of looking for possible Bachelor's or Master's thesis topics,"
 				+ "please do not hesitate to contacting me directly.";
 		iurl = "https://dbis.informatik.uni-kl.de/images/michel.png";
-		ngrams = Set.<String>of("computer science", "database systems", "information systems", "dbislab", "teaching", "bachelor thesis", "master thesis");
+		ngrams = Stream.of("computer science", "database systems", "information systems", "dbislab", "teaching", "bachelor thesis", "master thesis")
+		         .collect(Collectors.toCollection(HashSet::new));
 		budget = (float)10.0;
 		onclick = (float)0.01;
 		language = "eng";
@@ -140,7 +152,8 @@ public class ExampleAds {
 		url = "https://www.unisport.uni-kl.de/en/sports-program/sports-offers-a-z/swimming-ws21/22";
 		text = "It is important to do sports. Swimming is a very healthy sport. Try it out on Mondays and Thursdays.";
 		iurl = "https://www.unisport.uni-kl.de/fileadmin/_processed_/a/f/csm_Schwimmen_66fab68411.jpg";
-		ngrams = Set.<String>of("afterwork","sport","swimming", "unisport", "tukl");
+		ngrams = Stream.of("afterwork","sport","swimming", "unisport", "tukl")
+		         .collect(Collectors.toCollection(HashSet::new));
 		budget = (float)10.0;
 		onclick = (float)0.05;
 		language = "eng";
@@ -153,7 +166,8 @@ public class ExampleAds {
 		text = "E-Sports: Motor skills, tactical understanding and distinct problem-solving abilities, team spirit and communicative skills are the most important factors (and, of course, having a lot of fun!). "
 				+ "These skills must be understood in theory and developed in active gameplay.";
 		iurl = "https://www.unisport.uni-kl.de/fileadmin/_processed_/5/5/csm_E-Sports_10706bc684.jpg";
-		ngrams = Set.<String>of("computer science", "e sports", "free time", "afterwork", "fun", "unisport");
+		ngrams = Stream.of("computer science", "e sports", "free time", "afterwork", "fun", "unisport")
+		         .collect(Collectors.toCollection(HashSet::new));
 		budget = (float)2.0;
 		onclick = (float)0.5;
 		language = "eng";
